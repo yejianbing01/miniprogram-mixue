0ba62fc77dfa77b24dd0c0d60004dd25bf642380
yejb <yejb_01@163.com>
2023-07-23 23:33:50 +0800

    完成自定义tabbar

--- miniprogram/custom-tab-bar/index.ts
+++ miniprogram/custom-tab-bar/index.ts
@@ -1,3 +1,5 @@
+import ErrorMessage from "../enums/ErrorMessage"
+
 // custom-tab-bar/index.ts
 Component({
   /**
@@ -11,13 +13,53 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    active: 0,
+    list: [
+      {
+        text: '首页',
+        path: '/pages/index/index',
+        image: '../assets/images/tabbar/home.png',
+        selectedImage: '../assets/images/tabbar/home-selected.png'
+      },
+      {
+        text: '订餐',
+        path: '/pages/store/index',
+        image: '../assets/images/tabbar/store.png',
+        selectedImage: '../assets/images/tabbar/store-selected.png'
+      },
+      {
+        text: '订单',
+        path: '/pages/order/index',
+        image: '../assets/images/tabbar/order.png',
+        selectedImage: '../assets/images/tabbar/order-selected.png'
+      },
+      {
+        text: '我的',
+        path: '/pages/me/index',
+        image: '../assets/images/tabbar/me.png',
+        selectedImage: '../assets/images/tabbar/me-selected.png'
+      },
+    ]
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onChange(event: { detail: number }){
+      this.setData({ active: event.detail })
+      wx.switchTab({
+        url: this.data.list[event.detail].path
+      })
+    },
+    init(){
+      const currentPage = getCurrentPages().pop()
+      if(!currentPage){
+        throw new Error(ErrorMessage.INNER_ERROR)
+      }
+      this.setData({
+        active: this.data.list.findIndex(item=>item.path === `/${currentPage.route}`)
+      })
+    }
   }
 })
---
+++ miniprogram/enums/ErrorMessage.ts
@@ -0,0 +1,5 @@
+enum ErrorMessage {
+  INNER_ERROR = "内部错误"
+}
+
+export default ErrorMessage
